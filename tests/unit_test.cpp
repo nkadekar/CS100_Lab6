47344cd50ffbd9453b5a584f8a7e20fe33d39269
nkadekar <nkadekar7@gmail.com>
2020-11-18 11:51:20 -0800

    Added a couple more tests

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -160,6 +160,49 @@ TEST(SortTestSet, BubbleSortEmptyVector) {
 	ASSERT_EQ(container->size(), 0);
 }
 
+/*TEST(SortTestSet, BubbleSortTestList) {
+    Op* seven = new Op(7);
+    Op* four = new Op(4);
+    Mult* TreeA = new Mult(seven, four);
+
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Add* TreeB = new Add(three, two);
+
+    Op* ten = new Op(10);
+    Op* six = new Op(6);
+    Sub* TreeC = new Sub(ten, six);
+
+    ListContainer* container = new ListContainer();
+    container->add_element(TreeA);
+    container->add_element(TreeB);
+    container->add_element(TreeC);
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 28);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+    container->set_sort_function(new BubbleSort());
+    container->sort();
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 4);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 28);
+}
+
+TEST(SortTestSet, BubbleSortEmptyList) {
+	ListContainer* container = new ListContainer();
+	
+	ASSERT_EQ(container->size(), 0);
+
+	container->set_sort_function(new BubbleSort());
+	container->sort();
+
+	ASSERT_EQ(container->size(), 0);
+}*/
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
